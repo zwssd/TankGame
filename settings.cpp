5a7d9cd08f8705563a29b63fa32df57dcc618d21
ID:Kai <justko@foxmail.com>
2018-04-09 09:46:31 +0800

    Add files via upload

---
+++ settings.cpp
@@ -0,0 +1,10 @@
+#include "settings.h"
+int Settings::blockLength=30;       //方块边长(px)
+int Settings::life=1;                //生命(个)
+int Settings::width=20;              //地图宽度(个)
+int Settings::height=20;              //地图高度(个)
+int Settings::bulletSize=16;        //子弹边长(px)
+int Settings::bulletSpeed=160;       //子弹速度(px/s)
+int Settings::bulletRadius=8;
+int Settings::allEnemy=8;
+unsigned int Settings::maxExist=3;
